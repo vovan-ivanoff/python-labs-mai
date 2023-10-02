c1834909d8933e3a735946df0b5b232e08e8b30e
vovan-ivanoff <nnyo-yo@yandex.ru>
2023-10-03 00:38:53 +0300

    suka

--- 2.4/task_p.py
+++ 2.4/task_p.py
@@ -2,14 +2,17 @@ def task_p():
     """задание p: Редизайн таблицы умножения"""
     a = int(input())
     b = int(input())
-    for i in range(1, a + 1):
-        for j in range(1, b + 1):
-            if j != a:
-                print(f'{(str(i * j) + " " if b % 2 else str(i * j)).center(b)}|', end='')
-            else:
-                print(f'{(str(i * j) + " " if b % 2 else str(i * j)).center(b)}')
-        if i * j != a * a:
-            print(f'{"-"* ((b + 1) * a - 1)}')
+    for i in range(2, (a + 1) * 2 - 1):
+        result = ""
+        if i % 2 != 0:
+            print("-" * (a * b + (a - 1)))
+        else:
+            for k in range(2, (a + 1) * 2 - 1):
+                if k % 2 != 0:
+                    result += "|"
+                else:
+                    result += f"{(i // 2) * (k // 2):^{b}}"
+            print(result)
 
 
 if __name__ == '__main__':
